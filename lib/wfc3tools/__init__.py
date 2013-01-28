516e89e9cf31c504773727c65438312b82ff7a73
sienkiew <sienkiew@stsci.edu>
2013-01-28 18:18:14 +0000

    new install
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/wfc3tools@21839 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/wfc3tools/__init__.py
+++ lib/wfc3tools/__init__.py
@@ -8,27 +8,7 @@ module.
 
 """
 
-if False :
-    __version__ = ''
-
-    __svn_version__ = 'Unable to determine SVN revision'
-    __full_svn_info__ = ''
-    __setup_datetime__ = None
-
-    try:
-        __version__ = __import__('pkg_resources').\
-                            get_distribution('wfc3tools').version
-    except:
-        pass
-
-else :
-    __version__ = '1.0'
-
-try:
-    from wfc3tools.svninfo import (__svn_version__, __full_svn_info__,
-                                  __setup_datetime__)
-except ImportError:
-    pass
+from .version import *
 
 import runastrodriz
 import calwf3 
