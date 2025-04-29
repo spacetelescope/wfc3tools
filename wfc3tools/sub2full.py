5c18ada3bd261c5dab74768d872b9bfd4a64a336
Clare Shanahan <cshanahan@stsci.edu>
2025-04-29 16:57:48 -0400

    some tests

--- wfc3tools/sub2full.py
+++ wfc3tools/sub2full.py
@@ -85,7 +85,7 @@ def sub2full(filename, x=None, y=None, fullExtent=False):
 
     infiles, dummy_out = parseinput.parseinput(filename)
     if len(infiles) < 1:
-        return ValueError("Please input a valid HST filename")
+        raise ValueError("Please input a valid HST filename")
 
     coords = list()
 
@@ -112,10 +112,10 @@ def sub2full(filename, x=None, y=None, fullExtent=False):
             numcols = int(fd2[1].header['NUMCOLS'])
             fd2.close()
         except KeyError as e:
-            raise KeyError("Required header keyword missing; %s" % (e))
+            raise KeyError(f"Required header keyword missing: {e}")
 
         if "NO" in subarray:
-            raise ValueError("Image is not a subarray: %s" % (f))
+            raise ValueError(f"Image is not a subarray: {f}")
 
         sizaxis1 = numcols
         sizaxis2 = numrows
