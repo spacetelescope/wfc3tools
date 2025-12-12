1e46206afccb2365237d2ce2876335c43c9830d0
P. L. Lim <2090236+pllim@users.noreply.github.com>
2025-12-12 11:42:30 -0500

    Update test_pstat.py

--- wfc3tools/tests/test_pstat.py
+++ wfc3tools/tests/test_pstat.py
@@ -1,15 +1,12 @@
 import os
 
-from astroquery.mast import Observations
 import numpy as np
+from astroquery.mast import Observations
 
 from wfc3tools import pstat
 
 
-def test_pstat(tmpdir):
-
-    os.chdir(tmpdir)
-
+def test_pstat(_jail):
     filename = 'ibh719grq_ima.fits'
     Observations.download_file(f'mast:HST/product/{filename}', cache=True)
 
@@ -20,11 +17,13 @@ def test_pstat(tmpdir):
                        151.43222828, 138.52899031, 116.67495111, 109.19263377,
                        86.42073624, 80.97576756, 70.44724733, 53.37688116,
                        33.1249918, 12.25499919, -4.88105808, 0.]))
+    # MAST data can change over time, 1% agreement is good enough.
+    rtol = 0.01
 
     stat = pstat(filename, col_slice=(100, 104), row_slice=(20, 25),
                  units="counts", plot=False)
-    np.testing.assert_allclose(stat[0], truth[0])
-    np.testing.assert_allclose(stat[1], truth[1])
+    np.testing.assert_allclose(stat[0], truth[0], rtol=rtol)
+    np.testing.assert_allclose(stat[1], truth[1], rtol=rtol)
 
     stat = pstat(filename, col_slice=(100, 104), row_slice=(20, 25),
                  units="rate", plot=False)
@@ -32,5 +31,5 @@ def test_pstat(tmpdir):
                      2.10537601, 2.13960648, 2.02688408, 2.16728044,
                      2.00043583, 2.2481041, 2.44284439, 2.46466732,
                      2.28835583, 1.68012953, -43.30826569, 0.])
-    np.testing.assert_allclose(stat[0], truth[0])
-    np.testing.assert_allclose(stat[1], rate)
+    np.testing.assert_allclose(stat[0], truth[0], rtol=rtol)
+    np.testing.assert_allclose(stat[1], rate, rtol=rtol)
